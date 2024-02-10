e1241304bbefbc7c040373ab77941bdc7c08df2d
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-02-10 13:00:56 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,97 @@
+<script>
+    import "../app.css";
+  </script>
+  
+
+<div class="min-h-screen">
+    <header class="">
+        <div class="bg-[#4b83cc] py-3 container flex items-center justify-between px-32">
+            <div>
+                <img src="dwcl-logo.png" alt="">
+            </div>
+            <div class="flex flex-col items-end text-white">
+                <span>+63 52 480 2148</span>
+                <span>dwclinfo@dwc-legazpi.edu</span>
+                <ul class="grid grid-cols-3 gap-2 pt-2">
+                    <li>
+                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M20.9 2H3.1A1.1 1.1 0 0 0 2 3.1v17.8A1.1 1.1 0 0 0 3.1 22h9.58v-7.75h-2.6v-3h2.6V9a3.64 3.64 0 0 1 3.88-4a20.26 20.26 0 0 1 2.33.12v2.7H17.3c-1.26 0-1.5.6-1.5 1.47v1.93h3l-.39 3H15.8V22h5.1a1.1 1.1 0 0 0 1.1-1.1V3.1A1.1 1.1 0 0 0 20.9 2"/></svg></a>
+                    </li>
+    
+                    <li>
+                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="white" d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0 0 75-94a336.64 336.64 0 0 1-108.2 41.2A170.1 170.1 0 0 0 672 174c-94.5 0-170.5 76.6-170.5 170.6c0 13.2 1.6 26.4 4.2 39.1c-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 0 0-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 0 1-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 0 1-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9c-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850C671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2c33.2-24.3 62.3-54.4 85.5-88.2"/></svg></a>
+                    </li>
+                    <li>
+                        <a href=""> <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" fill-rule="evenodd" d="M7.465 1.066C8.638 1.012 9.012 1 12 1c2.988 0 3.362.013 4.534.066c1.172.053 1.972.24 2.672.511c.733.277 1.398.71 1.948 1.27c.56.549.992 1.213 1.268 1.947c.272.7.458 1.5.512 2.67C22.988 8.639 23 9.013 23 12c0 2.988-.013 3.362-.066 4.535c-.053 1.17-.24 1.97-.512 2.67a5.396 5.396 0 0 1-1.268 1.949c-.55.56-1.215.992-1.948 1.268c-.7.272-1.5.458-2.67.512c-1.174.054-1.548.066-4.536.066c-2.988 0-3.362-.013-4.535-.066c-1.17-.053-1.97-.24-2.67-.512a5.397 5.397 0 0 1-1.949-1.268a5.392 5.392 0 0 1-1.269-1.948c-.271-.7-.457-1.5-.511-2.67C1.012 15.361 1 14.987 1 12c0-2.988.013-3.362.066-4.534c.053-1.172.24-1.972.511-2.672a5.396 5.396 0 0 1 1.27-1.948a5.392 5.392 0 0 1 1.947-1.269c.7-.271 1.5-.457 2.67-.511m8.98 1.98c-1.16-.053-1.508-.064-4.445-.064c-2.937 0-3.285.011-4.445.064c-1.073.049-1.655.228-2.043.379c-.513.2-.88.437-1.265.822a3.412 3.412 0 0 0-.822 1.265c-.151.388-.33.97-.379 2.043c-.053 1.16-.064 1.508-.064 4.445c0 2.937.011 3.285.064 4.445c.049 1.073.228 1.655.379 2.043c.176.477.457.91.822 1.265c.355.365.788.646 1.265.822c.388.151.97.33 2.043.379c1.16.053 1.507.064 4.445.064c2.938 0 3.285-.011 4.445-.064c1.073-.049 1.655-.228 2.043-.379c.513-.2.88-.437 1.265-.822c.365-.355.646-.788.822-1.265c.151-.388.33-.97.379-2.043c.053-1.16.064-1.508.064-4.445c0-2.937-.011-3.285-.064-4.445c-.049-1.073-.228-1.655-.379-2.043c-.2-.513-.437-.88-.822-1.265a3.413 3.413 0 0 0-1.265-.822c-.388-.151-.97-.33-2.043-.379m-5.85 12.345a3.669 3.669 0 0 0 4-5.986a3.67 3.67 0 1 0-4 5.986M8.002 8.002a5.654 5.654 0 1 1 7.996 7.996a5.654 5.654 0 0 1-7.996-7.996m10.906-.814a1.337 1.337 0 1 0-1.89-1.89a1.337 1.337 0 0 0 1.89 1.89" clip-rule="evenodd"/></svg></a>
+                    </li>
+                   
+                    
+                    
+                </ul>
+            </div>
+        </div>
+        <nav class="mx-20">
+            <ul class="grid grid-flow-col font-semibold tracking-tighter text-center auto-cols-11">
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white bg-[#4b83cc] text-white"><a href="">HOME</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">ABOUT US</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">ADMISSION</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">ACADEMICS</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">STUDENT SERVICES</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">CARREER</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">GALLERY</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">UPDATES</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">CONTACT US</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">ALUMNI</a></li>
+                <li class="w-full py-3 hover:bg-[#4b83cc] hover:text-white"><a href="">LIBRARY</a></li>
+            </ul>
+        
+        </nav>
+    </header>
+    <slot />
+    <footer class="bg-[#4b83cc] text-white">
+        <div class="grid justify-center grid-cols-3">
+            <div class="flex flex-col px-20 gap-y-2">
+                <h6 class="py-2 text-xl font-bold">Our Campus Address</h6>
+                <span class="text-lg font-semibold">Nort Campus</span>
+                <p class="text-sm">Capt. F. Aquende Drive, Cruzada, <br/> Legazpi City 4500 Philippines</p>
+                <span class="text-lg font-semibold">South Campus</span>
+                <p class="text-sm">cor.J.P Rizal and Fr. J.L. Bates St., <br/> Albay District, Legazpi City 4500 <br/> Philippines</p>
+            </div>
+            <div class="flex flex-col gap-2 px-20">
+                <h6 class="py-2 text-xl font-bold">Our Contacts</h6>
+                <span class="text-lg font-semibold">TELEPHONE NUMBERS</span>
+                <span class="text-sm">+63 52 742-7966 (North Campus)</span>
+                <span class="text-sm">+63 52 480 2148 (South Campus)</span>
+                <span class="text-sm">480-3108 (Registrar)</span>
+                <span class="text-sm">0967 206 7429 (Marketing)</span>
+                <span class="font-semibold text-md">E-MAIL</span>
+                <a href="/" class="text-sm">dwclinfo@dwc-legazpi.edu</a>
+            </div>
+            <div class="flex flex-col gap-2 px-20">
+                <h6 class="py-2 text-xl font-bold">Our Portal</h6>
+                <a href="">Student</a>
+                <a href="">Faculty</a>
+                <a href="">DWCL-Library</a>
+                <span>SOCIAL MEDIA</span>
+                <ul class="flex items-center gap-2">
+                    <li>
+                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M20.9 2H3.1A1.1 1.1 0 0 0 2 3.1v17.8A1.1 1.1 0 0 0 3.1 22h9.58v-7.75h-2.6v-3h2.6V9a3.64 3.64 0 0 1 3.88-4a20.26 20.26 0 0 1 2.33.12v2.7H17.3c-1.26 0-1.5.6-1.5 1.47v1.93h3l-.39 3H15.8V22h5.1a1.1 1.1 0 0 0 1.1-1.1V3.1A1.1 1.1 0 0 0 20.9 2"/></svg></a>
+                    </li>
+    
+                    <li>
+                        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="white" d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0 0 75-94a336.64 336.64 0 0 1-108.2 41.2A170.1 170.1 0 0 0 672 174c-94.5 0-170.5 76.6-170.5 170.6c0 13.2 1.6 26.4 4.2 39.1c-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 0 0-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 0 1-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 0 1-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9c-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850C671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2c33.2-24.3 62.3-54.4 85.5-88.2"/></svg></a>
+                    </li>
+                    <li>
+                        <a href=""> <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" fill-rule="evenodd" d="M7.465 1.066C8.638 1.012 9.012 1 12 1c2.988 0 3.362.013 4.534.066c1.172.053 1.972.24 2.672.511c.733.277 1.398.71 1.948 1.27c.56.549.992 1.213 1.268 1.947c.272.7.458 1.5.512 2.67C22.988 8.639 23 9.013 23 12c0 2.988-.013 3.362-.066 4.535c-.053 1.17-.24 1.97-.512 2.67a5.396 5.396 0 0 1-1.268 1.949c-.55.56-1.215.992-1.948 1.268c-.7.272-1.5.458-2.67.512c-1.174.054-1.548.066-4.536.066c-2.988 0-3.362-.013-4.535-.066c-1.17-.053-1.97-.24-2.67-.512a5.397 5.397 0 0 1-1.949-1.268a5.392 5.392 0 0 1-1.269-1.948c-.271-.7-.457-1.5-.511-2.67C1.012 15.361 1 14.987 1 12c0-2.988.013-3.362.066-4.534c.053-1.172.24-1.972.511-2.672a5.396 5.396 0 0 1 1.27-1.948a5.392 5.392 0 0 1 1.947-1.269c.7-.271 1.5-.457 2.67-.511m8.98 1.98c-1.16-.053-1.508-.064-4.445-.064c-2.937 0-3.285.011-4.445.064c-1.073.049-1.655.228-2.043.379c-.513.2-.88.437-1.265.822a3.412 3.412 0 0 0-.822 1.265c-.151.388-.33.97-.379 2.043c-.053 1.16-.064 1.508-.064 4.445c0 2.937.011 3.285.064 4.445c.049 1.073.228 1.655.379 2.043c.176.477.457.91.822 1.265c.355.365.788.646 1.265.822c.388.151.97.33 2.043.379c1.16.053 1.507.064 4.445.064c2.938 0 3.285-.011 4.445-.064c1.073-.049 1.655-.228 2.043-.379c.513-.2.88-.437 1.265-.822c.365-.355.646-.788.822-1.265c.151-.388.33-.97.379-2.043c.053-1.16.064-1.508.064-4.445c0-2.937-.011-3.285-.064-4.445c-.049-1.073-.228-1.655-.379-2.043c-.2-.513-.437-.88-.822-1.265a3.413 3.413 0 0 0-1.265-.822c-.388-.151-.97-.33-2.043-.379m-5.85 12.345a3.669 3.669 0 0 0 4-5.986a3.67 3.67 0 1 0-4 5.986M8.002 8.002a5.654 5.654 0 1 1 7.996 7.996a5.654 5.654 0 0 1-7.996-7.996m10.906-.814a1.337 1.337 0 1 0-1.89-1.89a1.337 1.337 0 0 0 1.89 1.89" clip-rule="evenodd"/></svg></a>
+                    </li>
+                   
+                    
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="bg-black">
+            <h6 class="text-center ">Divine Word College of Legazpi © 2024</h6>
+        </div>
+    </footer>
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,84 @@
+<section class="relative min-h-[75vh] bg-bottom border border-black bg-[url('https://api.dwc-legazpi.edu/storage/images/slides/AV%20HALL%20Blessing.jpg')]">
+    <div class="absolute top-12 right-14">
+        <button class="px-4 py-2 bg-[#d2d2ee] text-[#4b83cc] text-xl font-semibold rounded-3xl">STUDEN PORTAL / ONLINE ENROLLMENT</button>
+    </div>
+    <div class="absolute top-0 bottom-0 left-0 flex items-center justify-center min-w-20">
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 15 15"><path fill="#494949" fill-rule="evenodd" d="M15 7.5a7.5 7.5 0 1 1-15 0a7.5 7.5 0 0 1 15 0m-8.5 2.707L3.793 7.5L6.5 4.793l.707.707l-1.5 1.5H11v1H5.707l1.5 1.5z" clip-rule="evenodd"/></svg>
+        </button>
+        
+    </div>
+    <div class="absolute top-0 bottom-0 right-0 flex items-center justify-center min-w-20">
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 15 15"><path fill="#494949" fill-rule="evenodd" d="M0 7.5a7.5 7.5 0 1 1 15 0a7.5 7.5 0 0 1-15 0m8.5-2.707L11.207 7.5L8.5 10.207L7.793 9.5l1.5-1.5H4V7h5.293l-1.5-1.5z" clip-rule="evenodd"/></svg>
+        </button>
+    </div>
+    
+
+
+</section>
+<div class="flex items-center justify-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="black"><path d="M224 128a96 96 0 1 1-96-96a96 96 0 0 1 96 96" opacity="0.2"/><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88"/></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="black"><path d="M224 128a96 96 0 1 1-96-96a96 96 0 0 1 96 96" opacity="0.2"/><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88"/></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="black"><path d="M224 128a96 96 0 1 1-96-96a96 96 0 0 1 96 96" opacity="0.2"/><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88"/></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="black"><path d="M224 128a96 96 0 1 1-96-96a96 96 0 0 1 96 96" opacity="0.2"/><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88"/></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><g fill="black"><path d="M224 128a96 96 0 1 1-96-96a96 96 0 0 1 96 96" opacity="0.2"/><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88"/></g></svg>
+</div>
+<section class="grid grid-cols-3 text-white border border-black min-h-80">
+    <div class="bg-[#389e61] px-10 hover:bg-[#35915a]">
+        <h3 class="text-2xl font-bold py-7">Why Choose Divine Word</h3>
+        <div class="flex flex-col pl-7">
+            <a href="" class="tracking-tight">Mission, Vision, Goal, Values</a>
+            <a href="" class="">History</a>
+            <a href="">School Facilities</a>
+        </div>
+    </div>
+    <div class="bg-[#5b5959] px-10 hover:bg-[#4e4c4d]">
+        <h3 class="text-2xl font-bold py-7">Academic Programs</h3>
+        <div class="flex flex-col gap-y-1 pl-7">
+            <a href="" class="tracking-tight">Grade School Department</a>
+            <a href="" class="">Junior High School Department</a>
+            <a href="">Free Secondary Discount Program</a>
+            <a href="">Senior High School Department</a>
+            <a href="">College</a>
+            <a href="">Graduate School of Business and Management</a>
+        </div>
+    </div>
+    <div class="bg-[#4b83cc] px-10 hover:bg-[#1510af]">
+        <h3 class="text-2xl font-bold py-7">Admission</h3>
+        <div class="flex flex-col gap-y-1 pl-7">
+            <a href="" class="tracking-tight">Grade School</a>
+            <a href="" class="">Junior High School</a>
+            <a href="">Free Secondary Distance Program</a>
+            <a href="">Senior High School</a>
+            <a href="">College</a>
+            <a href="">Graduate School</a>
+        </div>
+    </div>
+</section>
+<section class="grid grid-cols-2 p-20">
+    <div class="px-8 text-justify">
+        <h1 class="text-center text-[#4b83cc] font-semibold antialiased text-3xl pb-4">A Message From The President</h1>
+        <p>Welcome to Divine Word College of Legazpi. It matters to us that you know who we are and why this institution exists and will continue to exist.</p>
+        <br>
+        <p>The raison d’être of Divine Word College of Legazpi reflects the vision of Saint Arnold Janssen, the founder of the religious-missionary congregation – the Society of the Divine Word (SVD). Education is among the missionary apostolates of the Society. Our Founder, a secondary school teacher himself teaching natural sciences and mathematics, understood the importance of education as a method for evangelization, and as an effective way of missionary service. He impressed upon his missionaries that an integral approach in education is critical in the faith-formation of the person. To this day, we continue to live his vision and ensure that integral formation of the human person takes place in our schools. Our aim is to inform and mold individuals into “persons of the Word”, hearers and doers of the Word – in the language of the Divine Word Educational Association, "witnesses to the Word in the world".</p>
+        <br>
+        <p>To honor this commitment, here at Divine Word College of Legazpi, we value excellence, service, and professionalism. We also ascribe to the values of justice, peace and integrity of creation as an acknowledgment of our common origin as created beings, and our interrelatedness with one another through mutual respect and co-responsibility – hence, our being a family. These values are at the core of our day-to-day undertaking – a way of life, a pledge we honor and desire to render to our students and other stakeholders.</p>
+        <br>
+        <p>I invite you then to learn more about us. As you move through the pages of our website you will discover that our academic programs are designed to provide life skills and competencies that enable our young people to navigate confidently in the constantly changing milieu of the 21st century. Our commitment to academic excellence and holistic development nurtured in a healthy organizational environment and permeated with a missionary paradigm, our sense of corporate social responsibility and best practices in community involvement are meant to prepare our young to succeed in life as competent professionals with a Christian missionary character – ready for the world.</p>
+        <br>
+        <p>I wish you to be part of our family here at Divine Word College of Legazpi. We are here to serve. We give you our word.</p>
+        <br>
+        <p>In the Divine Word,</p>
+        <br>
+        <p>REV. FR. NIELO M. CANTILADO, SVD
+            <br>
+        DWCL President</p>
+    </div>
+    <div class="pt-4">
+        <img class="object-cover max-w-md translate-y-10" src="https://www.dwc-legazpi.edu/static/media/president.8c59a719.jpg" alt="">
+    </div>
+</section>
+<section class="flex items-center justify-center">
+    <iframe width="560" height="315" title="hehe" src="https://www.youtube.com/embed/KUNcP1H7Wi4" frameborder="0" allowfullscreen></iframe>
+</section>
